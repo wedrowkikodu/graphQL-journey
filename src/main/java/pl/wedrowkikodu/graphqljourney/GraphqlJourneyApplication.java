34654038bca4abf500c8943aad2eb1c340544af0
Rafał Boszkowicz <rafal.boszkowicz@gmail.com>
2024-01-27 18:27:36 +0100

    Init commit

---
+++ src/main/java/pl/wedrowkikodu/graphqljourney/GraphqlJourneyApplication.java
@@ -0,0 +1,13 @@
+package pl.wedrowkikodu.graphqljourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlJourneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/wedrowkikodu/graphqljourney/GraphqlJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wedrowkikodu.graphqljourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
